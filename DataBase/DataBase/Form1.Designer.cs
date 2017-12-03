299cb70f9ea631b5b9af5981fe9750590c47b970
svetluy <mikitn2009@yandex.ru>
2017-12-03 23:30:54 +0400

    Осталось дописать команды

--- DataBase/DataBase/Form1.Designer.cs
+++ DataBase/DataBase/Form1.Designer.cs
@@ -31,44 +31,103 @@
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(451, 252);
+            this.button1.Location = new System.Drawing.Point(338, 205);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(123, 57);
+            this.button1.Size = new System.Drawing.Size(149, 46);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Connect to DB";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(451, 172);
+            this.textBox1.Location = new System.Drawing.Point(339, 161);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(94, 22);
+            this.textBox1.Size = new System.Drawing.Size(72, 20);
             this.textBox1.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(448, 128);
+            this.label1.Location = new System.Drawing.Point(336, 123);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.Size = new System.Drawing.Size(29, 13);
             this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
+            this.label1.Text = "User";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(648, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.открытьToolStripMenuItem.Text = "Открыть";
+            this.открытьToolStripMenuItem.Click += new System.EventHandler(this.открытьToolStripMenuItem_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(415, 161);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(72, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(412, 123);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Password";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(671, 494);
+            this.ClientSize = new System.Drawing.Size(648, 401);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -79,6 +138,11 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- DataBase/DataBase/Form1.cs
+++ DataBase/DataBase/Form1.cs
@@ -1,14 +1,9 @@
-﻿using System;
+﻿using Oracle.DataAccess.Client; // ODP.NET Oracle managed provider
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using Oracle.DataAccess.Client; // ODP.NET Oracle managed provider
-using Oracle.DataAccess.Types;
+using System.Xml;
 
 namespace DataBase
 {
@@ -19,21 +14,74 @@ namespace DataBase
             InitializeComponent();
         }
 
+        List<XmlDocument> docs = new List<XmlDocument>();
+
         private void button1_Click(object sender, EventArgs e)
         {
             //$"Data Source=10.4.24.25:1522/orcl;User Id={userId};Password={password};"
-            string oradb = "Data Source = 127.0.0.1:1521; User Id = usr; Password =q12345678;";
-            
+            string oradb = $"Data Source = 127.0.0.1:1521; User Id = {textBox1.Text}; Password = {textBox2.Text};";
+            bool IsOpen;
+
             OracleConnection conn = new OracleConnection(oradb);  // C#
-            conn.Open();
-            OracleCommand cmd = new OracleCommand();
-            cmd.Connection = conn;
-            cmd.CommandText = "select CITY from OFFICES WHERE PLAN = 300000";
-            cmd.CommandType = CommandType.Text;
-            OracleDataReader dr = cmd.ExecuteReader();
-            dr.Read();
-            label1.Text = dr.GetString(0);
-            conn.Dispose();
+            try
+            {
+                conn.Open();
+                IsOpen = true;
+            }
+            catch (Exception e1)
+            {
+                MessageBox.Show(e1.Message);
+                IsOpen = false;
+            }
+
+            if (IsOpen)
+            {
+                OracleCommand cmd = new OracleCommand
+                {
+                    Connection = conn,
+                    CommandText = "CREATE TABLE XMLDOCS" +
+                  " (ИД NUMBER(5, 0) NOT NULL," +
+                  " ЗАЧЕТКА VARCHAR2(10) NOT NULL," +
+                  " ФИО VARCHAR2(30) NOT NULL," +
+                  " ГРУППА_ИД NUMBER(4, 0) NOT NULL," +
+                  " PRIMARY KEY(ИД)," +
+                  " FOREIGN KEY(ГРУППА_ИД) REFERENCES ГРУППЫ); ",
+                    CommandType = CommandType.Text
+                };
+
+                foreach (var xmlDoc in docs)
+                {
+                    cmd.CommandText = $"INSERT INTO XMLDOCS VALUES ({xmlDoc})";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Connection is not open");
+            }
+
+            //OracleDataReader dr = cmd.ExecuteReader();
+            //dr.Read();
+            //label1.Text = dr.GetString(0);
+            //conn.Dispose();
+        }
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string[] xmlDocs;
+            OpenFileDialog openFile = new OpenFileDialog
+            {
+                Filter = "xml docs (*.xml)|*.xml"
+            };
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                xmlDocs = openFile.FileNames;
+                foreach (var xmlDoc in xmlDocs)
+                {
+                    XmlDocument xDoc = new XmlDocument();
+                    xDoc.Load(xmlDoc);
+                    docs.Add(xDoc);
+                }
+            }
         }
     }
 }
