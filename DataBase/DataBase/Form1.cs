de160ba0fc7e9182acfd8914496f6f98d5b14e36
svetluy <mikitn2009@yandex.ru>
2017-12-03 22:38:08 +0400

    Робочая версия, она коннектится с ДБ

---
+++ DataBase/DataBase/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.DataAccess.Client; // ODP.NET Oracle managed provider
+using Oracle.DataAccess.Types;
+
+namespace DataBase
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //$"Data Source=10.4.24.25:1522/orcl;User Id={userId};Password={password};"
+            string oradb = "Data Source = 127.0.0.1:1521; User Id = usr; Password =q12345678;";
+            
+            OracleConnection conn = new OracleConnection(oradb);  // C#
+            conn.Open();
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "select CITY from OFFICES WHERE PLAN = 300000";
+            cmd.CommandType = CommandType.Text;
+            OracleDataReader dr = cmd.ExecuteReader();
+            dr.Read();
+            label1.Text = dr.GetString(0);
+            conn.Dispose();
+        }
+    }
+}
